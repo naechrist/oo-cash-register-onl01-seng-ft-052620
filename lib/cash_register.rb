0ee2d931fceb91d635b673172554b1a1ec776939
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-08 19:16:00 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,39 +1,39 @@
-class CashRegister
+#class CashRegister
  
- attr_accessor :total, :discount, :price, :items
+ #attr_accessor :total, :discount, :price, :items
  
- def initialize(discount = 0)
-   @total = 0 
-   @discount = discount
-   @items = [] 
-  end
+ #def initialize(discount = 0)
+  # @total = 0 
+  # @discount = discount
+   #@items = [] 
+  #end
   
-  def add_item(item, price, quantity = 1)
-    @price = price
-    @total += price * quantity
-    if quantity > 1 
-      counter = 0 
-      while counter < quantity
-      @items << items
-      counter += 1 
-      end
-    else 
-      @items << item 
-    end
-  end
+  #def add_item(item, price, quantity = 1)
+  #  @price = price
+   # @total += price * quantity
+   # if quantity > 1 
+   #   counter = 0 
+    #  while counter < quantity
+     # @items << items
+      #counter += 1 
+      #end
+  #  else 
+   #   @items << item 
+#    end
+ # end
   
-  def apply_discount
-    if @discount > 0 
-      @to_take_off = (price * discount)/100 
-      @total -= @to_take_off
-      return "After the discount, the total comes to $#{total}."
-    else 
-      return "There is no discount to apply."
-    end
-  end
+#  def apply_discount
+#    if @discount > 0 
+#      @to_take_off = (price * discount)/100 
+#      @total -= @to_take_off
+#      return "After the discount, the total comes to $#{total}."
+#    else 
+#      return "There is no discount to apply."
+#    end
+#  end
   
-  def void_last_tranaction
-    @total -= @price 
-  end
+#  def void_last_tranaction
+#    @total -= @price 
+#  end
   
-end
+#end
